edbad23633845a86b47ff7ff7ea656a90eec2937
rothen <rothen68@gmail.com>
2017-03-15 10:44:17 +0100

    Implements HoldButtonMenu with Bomb duration modification and Hold button modification

--- app/src/main/java/com/rothen/rbtimer/MainActivity.java
+++ app/src/main/java/com/rothen/rbtimer/MainActivity.java
@@ -1,15 +1,22 @@
 package com.rothen.rbtimer;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.CountDownTimer;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.rothen.rbtimer.fragments.EditTimeFragment;
 import com.rothen.rbtimer.fragments.HoldButtonFragment;
 import com.rothen.rbtimer.fragments.TimerFragment;
 import com.rothen.rbtimer.utils.Utils;
 
-public class MainActivity extends AppCompatActivity implements HoldButtonFragment.HoldButtonListener {
+public class MainActivity extends AppCompatActivity implements HoldButtonFragment.HoldButtonListener, EditTimeFragment.EditTimeDialogListener {
 
     private CountDownTimer buttonCountDown;
     private CountDownTimer bombCountDown;
@@ -17,8 +24,15 @@ public class MainActivity extends AppCompatActivity implements HoldButtonFragmen
     private TimerFragment timerFragment;
     private HoldButtonFragment holdButtonFragment;
 
-    private static final int BUTTONPRESSTIME = 5*1000;
-    private static final int BOMBTIME =  30*1000;
+    private static final int BUTTON_DEFAULT_TIME = 5*1000;
+    public static final String BUTTON_PRESS_TIME = "ButtonPressTime";
+
+    private int buttonPressTime;
+
+    private static final int BOMB_DEFAULT_TIME =  30*1000;
+    public static final String BOMB_TIME = "BombTime";
+
+    private int bombTime;
 
     private boolean isBombArmed;
     private boolean isBombExplosed;
@@ -38,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements HoldButtonFragmen
     {
         setBombDisarmed();
         isBombExplosed = false;
-        timerFragment.setTimer(Utils.millisecondToStringTimer(BOMBTIME));
+        timerFragment.setTimer(Utils.millisecondToStringTimer(bombTime));
     }
 
     @Override
@@ -47,16 +61,54 @@ public class MainActivity extends AppCompatActivity implements HoldButtonFragmen
         setContentView(R.layout.activity_main);
         timerFragment = (TimerFragment)  getSupportFragmentManager().findFragmentById(R.id.fragTimer);
         holdButtonFragment = (HoldButtonFragment) getSupportFragmentManager().findFragmentById(R.id.fragHoldButton);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        bombTime = prefs.getInt(BOMB_TIME, BOMB_DEFAULT_TIME);
+        buttonPressTime = prefs.getInt(BUTTON_PRESS_TIME,BUTTON_DEFAULT_TIME);
         resetBomb();
     }
 
     @Override
+    protected void onPause() {
+        SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(BOMB_TIME,bombTime);
+        editor.putInt(BUTTON_PRESS_TIME,buttonPressTime);
+        editor.apply();
+        super.onPause();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(!isBombArmed) {
+            switch (item.getItemId()) {
+                case R.id.mnuBombDuration:
+                    displayEditTime(bombTime / 1000, BOMB_TIME);
+                    return true;
+                case R.id.mnuHoldButtonDuration:
+                    displayEditTime(buttonPressTime / 1000, BUTTON_PRESS_TIME);
+                    return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    //HoldButtonFragment.HoldButtonListener
+    @Override
     public void holdButtonBeginTouch() {
         if(!isBombExplosed) {
-            buttonCountDown = new CountDownTimer(BUTTONPRESSTIME, 100) {
+            buttonCountDown = new CountDownTimer(buttonPressTime, 100) {
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    holdButtonFragment.setProgressState((int) (100 - (100 * millisUntilFinished / BUTTONPRESSTIME)));
+                    holdButtonFragment.setProgressState((int) (100 - (100 * millisUntilFinished / buttonPressTime)));
                 }
 
                 @Override
@@ -88,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements HoldButtonFragmen
     }
 
     private void startBombTimer() {
-        bombCountDown = new CountDownTimer(BOMBTIME,13) {
+        bombCountDown = new CountDownTimer(bombTime,13) {
             @Override
             public void onTick(long millisUntilFinished) {
                 timerFragment.setTimer(Utils.millisecondToStringTimer(millisUntilFinished));
@@ -112,4 +164,40 @@ public class MainActivity extends AppCompatActivity implements HoldButtonFragmen
 
         holdButtonFragment.setProgressState(0);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+
+
+    //EditTimeDialog
+    private void displayEditTime(int currentDuration, String valueType)
+    {
+        EditTimeFragment frag = new EditTimeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(EditTimeFragment.OLD_VALUE,currentDuration);
+        bundle.putString(EditTimeFragment.VALUE_TYPE, valueType);
+        frag.setArguments(bundle);
+        frag.show(getSupportFragmentManager(),"EditTime");
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog,String valueType, int value) {
+        if(valueType.equals(BOMB_TIME))
+        {
+            bombTime = value*1000;
+            resetBomb();
+        }
+        else if(valueType.equals(BUTTON_PRESS_TIME))
+        {
+            buttonPressTime = value*1000;
+        }
+    }
+
+
 }
